de78f4653fb6d5bdd5e6797b57a6c59579740749
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-04-22 16:14:01 +0200

    yes

--- main program.py
+++ main program.py
@@ -3,42 +3,45 @@ from battle_instance import *
 
 def main():
     with open("player data.csv", "r") as player_sheet_file:
-        player_sheet = csv.reader(player_sheet_file)
+        player_sheet = player_sheet_file.readlines()
 
         for player in player_sheet:
-            print(" ".join(player))
+            print(" ".join(player.split(",")))
 
-        action_list: str = "add player, battle" + "\n"
-        action: str = input(action_list).lower()
+    action_list: str = "add player, battle" + "\n"
+    action: str = input(action_list).lower()
 
-        while True:
-            match action:
-                case "battle":
-                    battle()
-                case "add player":
-                    add_player()
+    while True:
+        match action:
+            case "battle":
+                battle()
+            case "add player":
+                add_player()
+
+        with open("player data.csv", "r") as player_sheet_file:
+            player_sheet = player_sheet_file.readlines()
 
             for player in player_sheet:
-                print(" ".join(player))
+                print(" ".join(player.split(",")))
 
-            action: str = input(action_list).lower()
+        action: str = input(action_list).lower()
 
 
 def add_player():
     with open("player data.csv", "w", newline="") as player_data_file:
         player_csv = csv.writer(player_data_file, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
 
-        player_name: str = input("player name or exit")
+        player_name: str = input("player name or exit\n")
 
         while player_name != "exit":
-            speed: str = input("input speed")
+            speed: str = input("input speed\n")
 
             while speed.isnumeric() is False:
-                speed: str = input("input speed")
+                speed: str = input("input speed\n")
 
             player_csv.writerow([player_name, speed])
 
-            player_name = input("player name or exit")
+            player_name = input("player name or exit\n")
 
 
 if __name__ == "__main__":
