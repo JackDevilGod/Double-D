ceb013aa25a8204200af3b1eadce8ad3a866aa43
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2024-04-26 18:01:01 +0200

    yes

--- cyclopedia of heroes and monsters.py
+++ cyclopedia of heroes and monsters.py
@@ -4,12 +4,11 @@ player:
 monster:
 <name>,<hit points>,<defence>,<speed>,<strength>,<dexterity>,<constitution>,<intelligence>,<wisdom>,<charisma>,<skills>,<description>
 """
-Player_data_list: list[str] = ["<name>", "<race>", "<class","<hit points>", "<defence>", "<speed>", "<strength>", "<dexterity>",
-                               "<constitution>", "<intelligence>", "<wisdom>", "<charisma>", "<skills>",
-                               "<description>"]
+Player_data_list: list[str] = ["<name>", "<race>", "<class", "<hit points>", "<defence>", "<speed>", "<strength>", "<dexterity>",
+                               "<constitution>", "<intelligence>", "<wisdom>", "<charisma>", "<description>", "<skills>"]
 Monster_data_list: list[str] = ["<name>", "<hit points>", "<defence>", "<speed>", "<strength>", "<dexterity>",
-                                "<constitution>", "<intelligence>", "<wisdom>", "<charisma>", "<skills>",
-                                "<description>"]
+                                "<constitution>", "<intelligence>", "<wisdom>", "<charisma>", "<description>",
+                                "<skills>"]
 
 
 def main():
@@ -59,11 +58,34 @@ def add_entity(entity_type: str):
     with open(entity_type + " data.txt", "r") as sheet:
         data: list[str] = sheet.readlines()
 
-    with open(entity_type + " data.txt", "w") as sheet:
-        entity_name: str = input("name\n")
+    stats: list[str] = []
 
-        for stat in []:
-            pass
+    with (open(entity_type + " data.txt", "w") as sheet):
+        if entity_type == "heroes":
+            stat_list = Player_data_list
+        else:
+            stat_list = Monster_data_list
+
+        for index in range(len(stat_list)):
+            temp_stat = input(stat_list[index] + "\n")
+
+            while ((2 < index < 12 and temp_stat.isnumeric() is False and entity_type == "heroes") or
+                    (0 < index < 10 and temp_stat.isnumeric() is False and entity_type == "monsters")):
+                temp_stat = input(stat_list[index] + "\n")
+
+            if index < len(stat_list) - 1:
+                stats.append(temp_stat)
+
+            while index == len(stat_list) - 1 and temp_stat != "":
+                skill_description = input("describe skill\n")
+                stats.append(temp_stat + ":" + skill_description)
+
+                temp_stat = input("<skill> or enter to stop")
+
+        data.append(",".join(stats))
+
+        for line in data:
+            sheet.write(line + "\n")
 
 
 if __name__ == "__main__":
